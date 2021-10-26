dc45e8acf7d4dde14750c8ade83074321633ee77
adnan-mujagic <adnanmujagic@outlook.com>
2021-10-26 21:14:54 +0200

    Implementing LogIn/SignUp into MainTabNavigator

--- screens/LogInScreen.js
+++ screens/LogInScreen.js
@@ -12,15 +12,21 @@ import generalMainStyle from '../general_styles/generalMainStyle'
 import primaryColor from '../general_styles/primaryColor'
 import { setToken, setUser } from '../slices/mainSlice'
 import storeTokenToAsync from '../general_functions/storeTokenToAsync'
+import NumberPicker from '../components/NumberPicker'
 
 const LogInScreen = () => {
 
     const [credentials, setCredentials] = useState({
         username:"",
-        password:""
+        password:"",
+        email:"",
+        age:20,
+        full_name:""
     })
 
-    const [buttonDisabled, setButtonDisabled] = useState(false);
+    const [loginOrSignUp, setLoginOrSignUp] = useState("login");
+
+    const [buttonsDisabled, setButtonsDisabled] = useState(false);
 
     const dispatch = useDispatch();
 
@@ -34,36 +40,95 @@ const LogInScreen = () => {
         })
     } 
 
-    const onPress = async () => {
-        if(checkForEmptyFields(credentials)){
+    const onLogin = async () => {
+        let {username, password} = credentials;
+        if(checkForEmptyFields({username, password})){
             Alert.alert("Warning", "You must enter both username and password in order to proceed!")
         }
         else{
-            setButtonDisabled(true)
-            const res = await fetchDataWithoutAuth("/users/login","POST", credentials);
+            setButtonsDisabled(true)
+            const res = await fetchDataWithoutAuth("/users/login","POST", {username, password});
             if(res?.token){
                 await storeTokenToAsync(res.token);
                 dispatch(setToken(res.token));
                 const decoded = jwtDecode(res.token);
                 const userData = await getUserData(decoded.uid)
                 dispatch(setUser(userData));
-                navigation.navigate("MainTabNavigator")
+                navigation.navigate("Home")
             }
             else{
                 Alert.alert(res.status, "Please try again!")
-                setButtonDisabled(false)
+                setButtonsDisabled(false)
             }
         }
+        
+    }
+
+    const onAgeChange = (value) => {
+        setCredentials(prev => {
+            return {...prev, age: value}
+        })
+    }
+
+    const onRegister = async () => {
+        console.log("REGISTER PRESSED")
+        if(checkForEmptyFields(credentials)){
+            Alert.alert("Warning", "All the fields should be filled!")
+            return;
+        }
+        setButtonsDisabled(true);
+        const res = await fetchDataWithoutAuth("/users", "POST", credentials);
+        if(res?.data){
+            const loginInfo = await fetchDataWithoutAuth("/users/login", "POST", {username: credentials.username, password: credentials.password});
+            if(loginInfo?.token){
+                await storeTokenToAsync(loginInfo.token);
+                dispatch(setToken(loginInfo.token));
+                const decoded = jwtDecode(loginInfo?.token)
+                const user = await getUserData(decoded.uid);
+                if(user){
+                    dispatch(setUser(user));
+                    navigation.navigate("Home");
+                }
+                else{
+                    Alert.alert("Error", "Problem getting user data!")
+                }
+            }
+            else{
+                Alert.alert("Error", "Problem occured while trying to log in!")
+            }
+        }
+        else{
+            Alert.alert("Error", res.status)
+        }
+        setButtonsDisabled(false);
     }
 
     return (
         <View style={[generalMainStyle.topMarginAndFlex, {justifyContent:"center"}]}>
-            <View style={{margin:40, elevation:5, backgroundColor:"white", borderRadius:8}}>
+            {loginOrSignUp=="login" ?<View style={{margin:40, elevation:5, backgroundColor:"white", borderRadius:8}}>
                 <Text style={{marginHorizontal:10, marginTop:10, fontSize:18, fontWeight:"bold"}}>Enter your credentials</Text>
-                <CustomInput fieldName="username" placeholder={"Username..."} onChangeText={onChangeText}/>
-                <CustomInput fieldName="password" placeholder={"Password..."} password onChangeText={onChangeText}/>
-                <CustomButton title="LOG IN" onPress={onPress} type="solid" disabled={buttonDisabled}/>
+                <CustomInput value={credentials.username} fieldName="username" placeholder={"Username..."} onChangeText={onChangeText}/>
+                <CustomInput value={credentials.password} fieldName="password" placeholder={"Password..."} password onChangeText={onChangeText}/>
+                <CustomButton title="LOG IN" onPress={onLogin} type="solid" disabled={buttonsDisabled}/>
+                <TouchableOpacity onPress={()=>setLoginOrSignUp("sign-up")} style={{marginBottom:10, marginLeft:10}}>
+                    <Text style={{fontSize:16}}>Want to sign up?</Text>
+                </TouchableOpacity>
+            </View> :
+            <View style={{margin:40, elevation:5, backgroundColor:"white", borderRadius:8}}>
+                <Text style={{marginHorizontal:10, marginTop:10, fontSize:18, fontWeight:"bold"}}>Make an account</Text>
+                <CustomInput value={credentials.username} fieldName="username" placeholder={"Username..."} onChangeText={onChangeText}/>
+                <CustomInput value={credentials.password} fieldName="password" placeholder={"Password..."} password onChangeText={onChangeText}/>
+                <CustomInput value={credentials.email} fieldName="email" placeholder={"E-Mail..."} onChangeText={onChangeText}/>
+                <CustomInput value={credentials.full_name} fieldName="full_name" placeholder={"Full Name..."} onChangeText={onChangeText}/>
+                <NumberPicker value={credentials.age} setValue={onAgeChange}/>
+                <CustomButton title="REGISTER" onPress={onRegister} type="solid" disabled={buttonsDisabled}/>
+                <TouchableOpacity onPress={()=>setLoginOrSignUp("login")} style={{marginBottom:10, marginLeft:10}}>
+                    <Text style={{fontSize:16}}>Want to log in?</Text>
+                </TouchableOpacity>
             </View> 
+        
+        
+        }
         </View>
         
     )
--- screens/MainTabNavigator.js
+++ screens/MainTabNavigator.js
@@ -1,14 +1,32 @@
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { View, Text } from 'react-native'
 import primaryColor from '../general_styles/primaryColor';
 import Feed from './Feed';
 import HomeStackScreen from './HomeStackScreen';
 import Ionicons from 'react-native-vector-icons/Ionicons';
+import LogInScreen from './LogInScreen';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { useSelector } from 'react-redux';
+import { selectToken } from '../slices/mainSlice';
 
 const Tab = createBottomTabNavigator();
 
 const MainTabNavigator = () => {
+  const [loggedIn, setLoggedIn] = useState(false);
+
+  const t = useSelector(selectToken);
+
+    useEffect(()=>{
+      if(t){
+        setLoggedIn(true);
+      }else{
+        setLoggedIn(false)
+      }
+    },[t])
+
+    console.log(t)
+
     return (
         
           <Tab.Navigator
@@ -23,6 +41,10 @@ const MainTabNavigator = () => {
                 } else if (route.name === 'Feed') {
                   iconName = "md-compass-outline";
                 }
+
+                else if(route.name ==="LogIn"){
+                  iconName= focused? "log-in": "log-in-outline"
+                }
     
                 // You can return any component that you like here!
                 return <Ionicons name={iconName} size={size} color={color} />;
@@ -35,7 +57,7 @@ const MainTabNavigator = () => {
           >
             <Tab.Screen name="Home" component={HomeStackScreen} options={{headerShown:false}} />
             <Tab.Screen name="Feed" component={Feed} options={{headerShown:false}} />
-            
+            {!loggedIn && <Tab.Screen name="LogIn" component={LogInScreen} options={{headerShown:false}}/>}
           </Tab.Navigator>
       
     )
